2282e4f49904d1db4f88467b0619a5ec88fd3f07
TBF <troflog@gmail.com>
2022-11-01 23:52:18 +0100

    initial commit

---
+++ lua/options.lua
@@ -0,0 +1,8 @@
+local opt = vim.opt
+
+opt.expandtab = true                -- Use spaces instead of tabs
+opt.hidden = true                   -- Enable background buffers
+opt.ignorecase = true               -- Ignore case
+opt.joinspaces = false              -- No double spaces with join
+opt.relativenumber = true           -- Relative line numbers
+opt.clipboard = 'unnamedplus'      -- Using system clipboard
