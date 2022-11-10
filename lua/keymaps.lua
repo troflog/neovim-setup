4a2e00a46560f43e56ef8f5650f0fb972dd55470
troflog <troflog@gmail.com>
2022-11-10 19:26:49 +0100

    Update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,5 @@
 -- Avoid to repeat this in every mapping
 local options = {noremap = true}
 
-vim.g.mapleader = " " -- Map leader to space
+vim.g.mapleader = " "                    -- Map leader to space
 vim.keymap.set("i","jk","<Esc>",options) --Map jk to Esc
