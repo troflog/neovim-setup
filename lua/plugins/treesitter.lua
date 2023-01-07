8ae5a6f5954d5196a3260a7be30ab33e2b72e1c5
trond flogstad <troflog@gmail.com>
2023-01-07 10:44:18 +0100

    Removed print from treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-print('Hello from treesitter')
+-- print('Hello from treesitter')
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all"
   ensure_installed = { "c", "lua", "python" },
