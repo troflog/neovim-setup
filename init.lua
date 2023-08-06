604183d25454ddd9677dec0f03601d9ec1615053
TBF <troflog@gmail.com>
2023-08-06 19:28:38 +0200

    Fresh start with lazy

--- init.lua
+++
@@ -1,9 +0,0 @@
-require("plugins")
-require("utils")
-require("options")
-require("keymaps")
-require("color")
---require'lspconfig'.pyright.setup{}
---require('plugins.toggleterm')
-
---require('plugins.lsp') 
--- lua/plugins/dap/dap-python.lua
+++
@@ -1,4 +0,0 @@
-print('Hei dap python')
-require('dap-python').setup('~/miniconda3/envs/debugpy/bin/python')
-
-
