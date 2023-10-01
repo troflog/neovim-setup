eac79804c2738bd4f83161008392f4f5d7461b6d
flo <troflog@gmail.com>
2023-10-01 17:20:25 +0200

    Fixed error in treesitter K command

--- init.lua
+++ init.lua
@@ -2,5 +2,3 @@ require("lazyinstall")
 require("options")
 require("keymaps")
 
---vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,14 +1,45 @@
 return {
-    -- Git related plugins    
-    {'tpope/vim-fugitive'},
-    {'tpope/vim-rhubarb'},
+   -- Git related plugins    
+   {'tpope/vim-fugitive'},
+   {'tpope/vim-rhubarb'},
 
-    -- Detect tabstop and shiftwidth automatically
-    {'tpope/vim-sleuth'},
+   -- Detect tabstop and shiftwidth automatically
+   {'tpope/vim-sleuth'},
 
-    --Make it more easy to find commands
-    {'folke/which-key.nvim',    
-       opts = {}
+   --Make it more easy to find commands
+   {'folke/which-key.nvim',
+      opts = {}
+   },
+
+   -- Set lualine as statusline
+   {
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
     },
+   },
 
+   -- Add indentation guides even on blank lines
+   {
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help indent_blankline.txt`
+    main = "ibl",
+    opts = {},
+  },
+
+  -- "gc" to comment visual regions/lines
+  {
+      'numToStr/Comment.nvim',
+      opts = {
+          -- add any options here
+      },
+      lazy = false,
+  },
 }
