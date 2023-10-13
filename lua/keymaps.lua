68f9e874e026496a9f7912e64b114e8a3c313ce2
flo <troflog@gmail.com>
2023-10-13 20:56:31 +0200

    Removed some excessive keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -38,14 +38,11 @@ vim.keymap.set("n", "<leader>w\\", "<C-W>v", { desc = "Split window right", rema
 vim.keymap.set("n", "<leader>-"  , "<C-W>s", { desc = "Split window below", remap = true })
 vim.keymap.set("n", "<leader>\\" , "<C-W>v", { desc = "Split window right", remap = true })
 
+--Paste remaps--
 -- Paste from the * register 
 vim.keymap.set("n","ø",'"*p',{})
-vim.keymap.set("n","æ",'"*p',{})
-
---Paste remaps
 -- Delete into black hole register then paste (keep the pasted text in " registger)
 vim.keymap.set("x", "ø", [["_dP]])
 -- Copy into system clipboard
-vim.keymap.set({"n", "v"},"<leader>æ", [["*y]])
---Copy from system clipboard
-vim.keymap.set("n", "æ", [["*p]])
+-- vim.keymap.set({"n", "v"},"<leader>æ", [["*y]])
+
