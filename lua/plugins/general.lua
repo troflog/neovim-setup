afd364dbe106ea7c2036fba2d1787501acda9de7
Trond Fløgstad <troflog@gmail.com>
2024-02-03 13:18:48 +0100

    Tested autoformat

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -1,46 +1,47 @@
 return {
-   -- Git related plugins    
-   {'tpope/vim-fugitive'},
-   {'tpope/vim-rhubarb'},
+  -- Git related plugins
+  { 'tpope/vim-fugitive' },
+  { 'tpope/vim-rhubarb' },
 
-   -- Detect tabstop and shiftwidth automatically
-   {'tpope/vim-sleuth'},
+  -- Detect tabstop and shiftwidth automatically
+  { 'tpope/vim-sleuth' },
 
-   --Make it more easy to find commands
-   {'folke/which-key.nvim',
-      opts = {}
-   },
+  --Make it more easy to find commands
+  {
+    'folke/which-key.nvim',
+    opts = {}
+  },
 
-   -- Set lualine as statusline
-   {
-     'nvim-lualine/lualine.nvim',
-     -- See `:help lualine.txt`
-     opts = {
-        options = {
-          icons_enabled = false,
-          theme = 'onedark',
-          component_separators = '|',
-          section_separators = '',
-        },
-        sections = {
-          lualine_c = {
-            {
-              'filename',
-              file_status = true,      -- Displays file status (readonly status, modified status)
-              newfile_status = false,  -- Display new file status (new file means no write after created)
-              path = 2                 -- 1: Just the filename
-                                       -- 1: Relative path
-                                       -- 2: Absolute path
-                                       -- 3: Absolute path, with tilde as the home directory
-                                       -- 4: Filename and parent dir, with tilde as the home directory
-            },
+  -- Set lualine as statusline
+  {
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'onedark',
+        component_separators = '|',
+        section_separators = '',
+      },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            file_status = true,       -- Displays file status (readonly status, modified status)
+            newfile_status = false,   -- Display new file status (new file means no write after created)
+            path = 2                  -- 1: Just the filename
+            -- 1: Relative path
+            -- 2: Absolute path
+            -- 3: Absolute path, with tilde as the home directory
+            -- 4: Filename and parent dir, with tilde as the home directory
           },
         },
-     },
-   },
+      },
+    },
+  },
 
-   -- Add indentation guides even on blank lines
-   {
+  -- Add indentation guides even on blank lines
+  {
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
@@ -50,10 +51,10 @@ return {
 
   -- "gc" to comment visual regions/lines
   {
-      'numToStr/Comment.nvim',
-      opts = {
-          -- add any options here
-      },
-      lazy = false,
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    },
+    lazy = false,
   },
 }
